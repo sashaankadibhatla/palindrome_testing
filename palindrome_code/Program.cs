1d5c83cdd242939c719a3a339d3f60cfced921d3
sashaankadibhatla <sashaankadibhatla@gmail.com>
2017-09-25 18:04:57 +0530

    initial commit

---
+++ palindrome_code/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace palindrome_code
+{
+    class Program 
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string");
+            string str1 = Console.ReadLine();
+            
+            Palindrom palin= new Palindrom();
+            palin.IsPalindrom(str1);
+           
+            
+        }
+    }
+
+   public class Palindrom
+    {
+        public bool IsPalindrom(string str)
+        {
+            string revrse="";
+           
+             for (int i = str.Length-1; i >=0; i--) //String Reverse
+            {
+                revrse += str[i].ToString();
+            }
+            if (revrse == str) 
+            {   
+                Console.WriteLine("String is a palindrome");
+                return true;
+            }
+            else
+            {   
+                Console.WriteLine("String is not a palindrome");
+                return false;
+            }
+           
+        }
+    }
+}
